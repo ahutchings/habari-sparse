f110fdc9264a6d6cbd3f1ff5ef09d4be52ddb44d
Andrew Hutchings <andrew.hutchings@gmail.com>
2009-11-19 02:02:26 -0500

    Remove article header information for pages.

--- page.single.php
+++ page.single.php
@@ -1,11 +1,6 @@
 <?php $theme->display('header') ?>
 
     <article>
-        <header>
-            <h2><a href="<?php echo $post->permalink ?>" rel="bookmark"><?php echo $post->title ?></a></h2>
-            <time datetime="<?php echo $post->pubdate->text_format('{Y}-{m}-{d}T{H}:{i}:{s}{O}') ?>"><?php echo $post->pubdate->text_format('{N}{S} {F} {Y}') ?></time>
-            <span class="tags">tags: <?php echo $post->tags_out ?></span>
-        </header>
         <?php echo $post->content_out ?>
     </article>
 

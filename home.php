bd3f176d25772b214f68b3929609dec85c8289b2
Andrew Hutchings <andrew.hutchings@gmail.com>
2009-12-24 22:14:37 -0500

    Only display "tags" text for entries with tags.

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
         <header>
             <h2><a href="<?php echo $post->permalink ?>" rel="bookmark"><?php echo $post->title_out ?></a></h2>
             <time datetime="<?php echo $post->pubdate->text_format('{Y}-{m}-{d}T{H}:{i}:{s}{P}') ?>"><?php echo $post->pubdate->text_format('{j}{S} {F} {Y}') ?></time>
-            <span class="tags">tags: <?php echo $post->tags_out ?></span>
+            <?php if ($post->tags): ?><span class="tags">tags: <?php echo $post->tags_out ?></span><?php endif ?>
         </header>
         <?php echo $post->content_out ?>
     </article>
